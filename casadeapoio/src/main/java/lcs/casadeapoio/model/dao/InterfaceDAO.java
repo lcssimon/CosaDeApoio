9b68de14b6234c8aa76cec108c927e9e7e646693
user01 <user01@Lcssimon-PC>
2013-09-02 00:28:05 -0300

    Criação da InterfaceDAO

---
+++ casadeapoio/src/main/java/lcs/casadeapoio/model/dao/InterfaceDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lcs.casadeapoio.model.dao;
+
+/**
+ *
+ * @author user01
+ */
+public interface InterfaceDAO {
+    
+}
